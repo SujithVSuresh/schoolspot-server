455885a968cc2fc0aa024e476486f9de21e13ea0
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-26 13:12:13 +0530

    feat: crated error handling middleware

---
+++ src/middlewares/ErrorHandler.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express"
+import { CustomError } from "../utils/CustomError";
+
+export const errorHandler = (err: CustomError , req: Request, res: Response, next: NextFunction) => {
+    let statusCode = 500;
+    let message = "Internal Server Error";
+
+    if(err instanceof CustomError){
+        statusCode = err.statusCode;
+        message = err.message;
+    }
+
+    res.status(statusCode).json({
+        status: statusCode,
+        message: message
+    })
+}
