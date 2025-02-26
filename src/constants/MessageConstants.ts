9c6ef1f0c60d6980966e30aff1115f4c0b7c8337
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-26 13:07:46 +0530

    feat: add message constants and status constants

---
+++ src/constants/MessageConstants.ts
@@ -0,0 +1,22 @@
+const Messages = {
+    SERVER_ERROR: "Internel server error",
+    USER_EXIST: "User already exist",
+    USER_NOT_FOUND: "User with this email doesn't exist",
+    OTP_EXPIRY: "Your OTP has expired",
+    INVALID_OTP: "OTP is not valid",
+    INVALID_EMAIL: "Please enter a valid email",
+    PASSWORD_LENGTH: "Your password must be at least 8 characters long",
+    PASSWORD_LETTER: "Your password must contain at least one letter",
+    PASSWORD_DIGIT: "Your password must contain at least one number",
+    PASSWORD_SPECIALCHAR: "Your password must contain at least one special character",
+    ACCOUNT_NOT_FOUND: "User with this credentials doesn't exist",
+    TOKEN_EXPIRED: "Password reset link has been expired",
+    PROFILE_ALREADY_EXIST: "Profile for this user already exist",
+    PROFILE_NOT_FOUND: "Profile for this user doesn't exist",
+    ACCESS_NOT_ALLOWED: "Access denied, No token provided",
+    JWT_TOKEN_EXPIRED: "Authentication token has expired",
+    INVALID_TOKEN: "Token is invalid",
+    NO_TOKEN: "No token provided"
+  };
+
+  export default Messages
