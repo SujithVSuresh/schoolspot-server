b67a0a8791a0821aa082bdc96720c8b971319ef5
SujithVSuresh <sujitheternal13@gmail.com>
2025-02-26 15:35:20 +0530

    feat: add verify user and resend otp to auth service interface

--- src/services/interface/IAuthService.ts
+++ src/services/interface/IAuthService.ts
@@ -1,9 +1,10 @@
-
-
 import { UserType } from "../../types/types";
 
 
 
 export interface IAuthService{
     signup(user: UserType): Promise<string>
+    verify(otpCode: number, email: string): Promise<Partial<UserType>>
+    resendOtp(email: string): Promise<string>
+
 }
